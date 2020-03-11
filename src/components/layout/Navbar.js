2bfe8866a7bac214d1386318aa0b54c1848cc956
Brett Saxon <brettcsaxon@gmail.com>
2020-03-11 15:38:40 -0400

    working on styled components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = props => {
-    return (
-        <nav>
-            <h1>
-                <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/concerts'>Concerts</Link>
-                    </li>
-                </ul>
-            </h1>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/concerts'>Concerts</Link>
+          </li>
+        </ul>
+      </h1>
+    </nav>
+  );
+};
 
 export default Navbar;
